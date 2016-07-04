6c56d517a2a0b2219b98713f96009f7aa767a4be
zxx1988328 <297208251@qq.com>
2016-07-04 21:35:37 +0800

    edit the path

--- imooc/app.js
+++ imooc/app.js
@@ -12,14 +12,14 @@ app.get('/',function(req,res){
 	res.render('index',{title:'首页'})
 })
 
-app.get('/',function(req,res){
+app.get('/movie/:id',function(req,res){
 	res.render('detail',{title:'详情页'})
 })
 
-app.get('/',function(req,res){
+app.get('/admin/movie',function(req,res){
 	res.render('admin',{title:'后台录入页'})
 })
 
-app.get('/',function(req,res){
+app.get('/admin/list',function(req,res){
 	res.render('list',{title:'列表页'})
 })
