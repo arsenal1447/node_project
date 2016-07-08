ef27452598282db880ab23fb680c8aa49480d926
xinxin <xinxin@rrkd.cn>
2016-07-08 16:15:41 +0800

    修复删除的bug

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,17 +1,17 @@
-$(function(){
-    $('.del').click(function(e){
-        var target = $(e.target)
-        var id = target.data('id')
-        var tr = $('.item-id-'+ id)
+$(function () {
+    $('.del').click(function (e) {
+        var target = $(e.target);
+        var id = target.data('id');
+        var tr = $('.item-id-' + id);
 
         $.ajax({
-            type:'DELETE',
-            url:'/admin/list?id='+ id
+            type: 'DELETE',
+            url: '/admin/list?id='+ id
         })
-        .done(function(results){
-            if(results.status===1){
-                if(tr.length>0){
-                    tr.remove()
+        .done(function (results) {
+            if (results.success ) {
+                if (tr.length > 0) {
+                    tr.remove();
                 }
             }
         })
